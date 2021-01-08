a2b4b9048ddc74c6cea239359de59bc19a43b04e
yxpoe2016 <535607746@qq.com>
2021-01-08 17:36:09 +0800

    loading login预制体，脚本

---
+++ Src/Client/Assets/Scripts/UI/UIRegister.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRegister : MonoBehaviour
+{
+
+    public InputField username;
+
+    public InputField password;
+
+    public InputField passwordConfirm;
+
+    public Button buttonRegister;
+
+	// Use this for initialization
+	void Start () {
+        buttonRegister.onClick.AddListener(OnClickRegister);
+
+    }
+
+    void OnRegister()
+    {
+
+    }
+
+    public void OnClickRegister()
+    {
+        if (string.IsNullOrEmpty(username.text))
+        {
+            MessageBox.Show("请输入账号");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(password.text))
+        {
+            MessageBox.Show("请输入密码");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(passwordConfirm.text))
+        {
+            MessageBox.Show("请输入确认密码");
+            return;
+        }
+
+        if (password.text!=passwordConfirm.text)
+        {
+            MessageBox.Show("两次输入密码不一致");
+            return;
+        }
+
+    }
+}
